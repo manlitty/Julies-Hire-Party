ba8a963d7a48359aab036756057fccb3413d9f4a
Rylan Clark Iglesia <st22282@ormiston.school.nz>
2023-05-17 11:28:24 +1200

    minor changes

--- Julie's Party Hire -.py
+++ Julie's Party Hire -.py
@@ -1,27 +1,35 @@
 #Julie's Party Hire
 
+import tkinter as tk
 from tkinter import *
-from PIL import Image, ImageTk #still comes out yellow
+from tkinter import font
 
-root=Tk()
-root.title("Julie's Hire Party")
-root.iconbitmap('C:\\Users\\super\\Downloads\\hireparty.ico')
-root.minsize(height=450,width=500)
+#from PIL import Image, #still comes out yellow
+
+win = tk.Tk()
+win.title("Julie's Hire Party")
+win.iconbitmap('C:\\Users\\super\\Downloads\\hireparty.ico')
+win.minsize(height=450,width=500)
+
+style1 = font.Font(size=25)
+style2 = font.Font(size=20)
+style3 = font.Font(size=15)
+
 
 # Window 1 - Opening Window
 
 def window_1():
-    label1=Label(root, text="Welcome to...", font=("Century Gothic",35))
-    label1.place(relx= 0.0, rely = 0.0,  anchor = 'nw')
-    button1=Button(root,text='Proceed',font=('Catamaran',15),command=window_2)
+    label1=Label(window_1, text="Welcome to...", font=("Century Gothic",35))
+    label1.pack(relx= 0.0, rely = 0.0,  anchor = 'nw')
+    button1=Button(window_1,text='Proceed',command=lambda: window_2.tkraise(), font=style2)
     button1.pack(side=BOTTOM)
     
     #Images - 21-25
-    party_Rentals = ImageTk.PhotoImage(Image.open("julies_party_rentals.png"))
+    party_Rentals = tk.PhotoImage(file = "julies_party_rentals.png")
    
-    label2 = Label(root, image = party_Rentals)
+    label2 = Label(win, image = party_Rentals)
     
-    label2 = Label(root, image= party_Rentals, bd=3, relief="sunken")
+    label2 = Label(win, image= party_Rentals, bd=3, relief="sunken")
     
     label2.pack(padx=20, pady=75 )
     
@@ -30,13 +38,12 @@ def window_1():
         
         label1.destroy()
         button1.destroy()
-        label3= Label(root,text="Julie's Party Hire", font=('Century Gothic',15))
-        button3 = Button(root, text='Exit', font=('Century Gothic',15),command=back)
+        label3= Label(window_2,text="Julie's Party Hire", font=('Century Gothic',15))
+        button3 = Button(window_2, text='Exit', font=('Century Gothic',15),command=back)
         label3.pack()
         button3.pack(side=RIGHT)
         def back():
             label3.destroy()
             button3.destroy()
-            window_1()
 window_1()
 root.mainloop()
--- Switching Pages.py
+++ Switching Pages.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Switching Between Pages
 
 import tkinter as tk
@@ -57,60 +56,4 @@ page1.tkraise()
 win.geometry("650x650")
 win.title("Multiple Pages Application")
 win.resizable(False, False)
-=======
-# Switching Between Pages
-
-import tkinter as tk
-from tkinter import *
-from tkinter import font
-
-win = tk.Tk()
-
-style1 = font.Font(size=25)
-style2 = font.Font(size=20)
-style3 = font.Font(size=15)
-
-
-page1 = Frame(win)
-page2 = Frame(win)
-page3 = Frame(win)
-
-page1.grid(row=0, column=0, sticky="nsew")
-page2.grid(row=0, column=0, sticky="nsew")
-page3.grid(row=0, column=0, sticky="nsew")
-
-lb1 = Label (page1, text="I am page 1", font=style1)
-lb1.pack(pady=20)
-
-lb2 = Label (page2, text="I am page 2", font=style1)
-lb2.pack(pady=30)
-
-lb3 = Label (page3, text="I am page 3", font=style1)
-lb3.pack(pady=50)
-
-
-btn1 = Button(page1, text="Show Page 2", command=lambda: page2.tkraise(), font=style2)
-btn1.pack()
-
-
-btn2 = Button(page2, text="Show Page 1", command=lambda: page1.tkraise(), font=style2)
-btn3 = Button(page2, text="Show Page 3", command=lambda: page3.tkraise(), font=style2)
-
-btn2.pack()
-btn3.pack()
-
-btn4 = Button(page3, text="Show Page 1", command=lambda: page1.tkraise(), font=style2)
-btn5 = Button(page3, text="Show page 2", command=lambda: page2.tkraise(), font=style2)
-
-btn4.pack()
-btn5.pack()
-
-
-page1.tkraise() 
-
-
-win.geometry("650x650")
-win.title("Multiple Pages Application")
-win.resizable(False, False)
->>>>>>> 3bcce7e6ac08074e937f81e7b23f770d3afd8942
 win.mainloop()
