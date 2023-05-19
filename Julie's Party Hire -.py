4c44d0ad0e1e77e5f7074239deb8150af000fc33
Rylan Clark Iglesia <st22282@ormiston.school.nz>
2023-05-19 13:59:09 +1200

    additional PIL it works

--- Julie's Party Hire -.py
+++ Julie's Party Hire -.py
@@ -1,49 +1,50 @@
 #Julie's Party Hire
-
-import tkinter as tk
 from tkinter import *
-from tkinter import font
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+import tkinter
 
-#from PIL import Image, #still comes out yellow
-
-win = tk.Tk()
-win.title("Julie's Hire Party")
-win.iconbitmap('C:\\Users\\super\\Downloads\\hireparty.ico')
-win.minsize(height=450,width=500)
-
-style1 = font.Font(size=25)
-style2 = font.Font(size=20)
-style3 = font.Font(size=15)
-
+root=Tk()
+root.title("Julie's Hire Party")
+root.iconbitmap('C:\\Users\\super\\Downloads\\hireparty.ico')
+root.minsize(height=450,width=500)
 
 # Window 1 - Opening Window
 
 def window_1():
-    label1=Label(window_1, text="Welcome to...", font=("Century Gothic",35))
-    label1.pack(relx= 0.0, rely = 0.0,  anchor = 'nw')
-    button1=Button(window_1,text='Proceed',command=lambda: window_2.tkraise(), font=style2)
+    label1=Label(root, text="Welcome to...", font=("Century Gothic", 35))
+    button1=Button(root,text='Proceed',font=('Catamaran',15))
+    label1.place(relx= 0.0, rely = 0.0)
+    button1=Button(root,text='Proceed',font=('Catamaran',15))
     button1.pack(side=BOTTOM)
-    
+
     #Images - 21-25
-    party_Rentals = tk.PhotoImage(file = "julies_party_rentals.png")
-   
-    label2 = Label(win, image = party_Rentals)
+    img = ImageTk.PhotoImage(Image.open("C:\\Users\\super\\Downloads\\JULIES_HIRE_PARTY\\PROGRAM\\imagesss.png"))
+    testlabel = Label(image=img)
+    img = Image.resize((100,100), Image.ANTIALIAS)
+    testlabel.pack()
     
-    label2 = Label(win, image= party_Rentals, bd=3, relief="sunken")
-    
-    label2.pack(padx=20, pady=75 )
-    
-    
+    imagelabel = Label(root, image=img).place(x=100, y=200)
+
+
     def window_2():#page 2
-        
+
         label1.destroy()
         button1.destroy()
-        label3= Label(window_2,text="Julie's Party Hire", font=('Century Gothic',15))
-        button3 = Button(window_2, text='Exit', font=('Century Gothic',15),command=back)
+        label2= Label(root,text='Night', font=('Century Gothic',15))
+        button2 = Button(root, text='Exit', font=('Century Gothic',15),command=back)
+        label2.pack()
+        button2.pack(side='right')
+        label3= Label(root,text="Julie's Party Hire", font=('Century Gothic',15))
+        button3 = Button(root, text='Exit', font=('Century Gothic',15),command=back)
         label3.pack()
         button3.pack(side=RIGHT)
         def back():
+            label2.destroy()
+            button2.destroy()
             label3.destroy()
             button3.destroy()
+            window_1()
 window_1()
 root.mainloop()
