8ef9e0cfad01755d6b88a1fc1ff0f1b8118386c0
Rylan Clark Iglesia <st22282@ormiston.school.nz>
2023-05-22 20:43:00 +1200

    2nd window commit

--- Julie's Party Hire -.py
+++ Julie's Party Hire -.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 import tkinter
+from tkinter import messagebox
 
 root=Tk()
 root.title("Julie's Hire Party")
@@ -17,37 +18,44 @@ image_references = []
 # Window 1 - Opening Window
 
 def window_1():
-    label1=Label(root, text="Welcome to...", font=("Century Gothic", 35))
-    label1.place(relx= 0.0, rely = 0.0)
-    button1=Button(root,text='Proceed', font=('Catamaran',15))
-    button1.pack(side=BOTTOM)
-
-    #Images - 21-25
-    img = Image.open("C:\\Users\\super\\Downloads\\JULIES_HIRE_PARTY\\PROGRAM\\imagesss.png")
-    img2 = img.resize((300,300), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(img2)
-    imagelabel = Label(root, image=photo)
-    imagelabel.pack()
-    image_references.append(photo)
-    imagelabel.place(x=100, y=100)
-
-    def window_2():#page 2
-
+    def window_2():
         label1.destroy()
         button1.destroy()
-        label2= Label(root,text='Night', font=('Century Gothic',15))
-        button2 = Button(root, text='Exit', font=('Century Gothic',15),command=back)
-        label2.pack()
-        button2.pack(side='right')
-        label3= Label(root,text="Julie's Party Hire", font=('Century Gothic',15))
-        button3 = Button(root, text='Exit', font=('Century Gothic',15),command=back)
-        label3.pack()
-        button3.pack(side=RIGHT)
+        imagelabel.destroy()
+        frame.destroy()
+        for image_ref in image_references:
+            image_ref.__del__()
+        
+        #Labels and buttons for 2nd Window
+        label2=Label(root, text="WORK PLEASe", font=("Century Gothic", 25))
+        label2.place(relx=.0, rely=0.0, anchor='nw')
         def back():
             label2.destroy()
             button2.destroy()
-            label3.destroy()
-            button3.destroy()
             window_1()
+        button2=Button(root,text='Proceed', font=('Catamaran',15),command=back)
+        button2.place(relx=1.0, rely=1.0, anchor='se')
+    button1=Button(root,text='Proceed', font=('Catamaran',15))
+    button1.place(relx=1.0, rely=1.0, anchor='se')
+    #Window_1 Labels, Buttons etc.    
+    label1=Label(root, text="Welcome to...", font=("Century Gothic", 25))
+    label1.place(relx= 0.0, rely = 0.0)
+    button1=Button(root,text='Proceed', font=('Catamaran',15),command=window_2,activebackground='blue')
+    button1.place(relx=1.0, rely=1.0, anchor='se')
+
+
+    #Images
+    img = Image.open("C:\\Users\\super\\Downloads\\JULIES_HIRE_PARTY\\PROGRAM\\imagesss.png")
+    img2 = img.resize((350,350), Image.ANTIALIAS)
+    photo = ImageTk.PhotoImage(img)
+    imagelabel = Label(root, image=photo)
+    image_references.append(photo)
+    imagelabel.place(x=100, y=100)
+    #Framing for Image
+    frame = Frame(root, highlightbackground='black', highlightthickness=2)
+    frame.pack(pady=50)
+    imagelabel2 = Label(frame, image=photo)
+    imagelabel2.pack()
+    
 window_1()
 root.mainloop()
