052bee02a58995205789906b994f29bb98aca626
Rylan Clark Iglesia <st22282@ormiston.school.nz>
2023-05-26 11:57:03 +1200

    entry widget commit

--- Julie's Party Hire -.py
+++ Julie's Party Hire -.py
@@ -11,7 +11,7 @@ from tkinter import messagebox
 root=Tk()
 root.title("Julie's Hire Party")
 root.iconbitmap('C:\\Users\\super\\Downloads\\hireparty.ico')
-root.minsize(height=450,width=500)
+root.minsize(height=450,width=650)
 
 image_references = []
 
@@ -20,54 +20,55 @@ image_references = []
 # Window 1 - Opening Window
 
 def window_1():
+    
     #Window 2
     
     def window_2():
-        label1.destroy()
-        button1.destroy()
-        imagelabel.destroy()
-        frame.destroy()
+        label1.destroy() #destroys Label1
+        button1.destroy() #destroy button from window 1
+        imagelabel.destroy() #destroy image label from window 1
+        frame.destroy() # destroys framing from window 1
+        
         #Delete Startup Image
         for image_ref in image_references:
             image_ref.__del__()
+            
+        #Entry Widgets
+        L1 = Label(text="What's Your Name?:", font=40)
+        L1.grid(row=0, column=0) 
         
+        E1=Entry(root,bg="#f4f3f8")
+        E1.grid(row=1, column=0, padx=(0,45))
         
-        my_tree = ttk.Treeview(root, columns=("Row_#", "Your_Name", "Reciept_Number", "Item_Number_Hired", "Item_Hired")
-                               ,show="headings")
-        my_tree.heading("Row_#", text="Row No.")
-        my_tree.heading("Your_Name", text="Customer Name")
-        my_tree.heading("Reciept_Number", text="Reciept no.")
-        my_tree.heading("Item_Number_Hired", text="No. of items hired")
-        my_tree.heading("Item_Hired", text="Item Name")
-        my_tree.grid(row=2, column=0, columnspan=100, padx=0, pady=100)
+        L2 = Label(text="Your Reciept Number:", font=10)
+        L2.grid(row=2, column=0, padx=(0,30))
         
+        E2=Entry(root,bg="#f4f3f8")
+        E2.grid(row=3, column=0, padx=(0,45))
+        
+        L3 = Label(text="Name of Items Hired:", font=40)
+        L3.grid(row=4, column=0, padx=(0,50))
+        
+        E3=Entry(root,bg="#f4f3f8")
+        E3.grid(row=5, column=0, padx=(0,45))
+        
+        L4 = Label(text="Number of Items Hired:", font=20)
+        L4.grid(row=6, column=0, padx=(0,50))
+        
+        E4=Entry(root,bg="#f4f3f8")
+        E4.grid(row=7, column=0, padx=(0,45))
         
 
+        #Using Treeview Widget
+        #my_tree = ttk.Treeview(root, columns=("Row_#", "Your_Name", "Reciept_Number", "Item_Number_Hired", "Item_Hired")
+                               #,show="headings")
+        #my_tree.heading("Row_#", text="Row No.")
+        #my_tree.heading("Your_Name", text="Customer Name")
+        #my_tree.heading("Reciept_Number", text="Reciept no.")
+        #my_tree.heading("Item_Number_Hired", text="No. of items hired")
+        #my_tree.heading("Item_Hired", text="Item Name")
+        #my_tree.grid(row=2, column=0, columnspan=100, padx=0, pady=100)
         
-        
-        #Define Columns
-        
-        
-        #Columns
-        
-        #my_tree.column("Row #", width=120, minwidth=25)
-        #my_tree.column("Your Name", anchor=E, width=120)
-        #my_tree.column("Reciept Number", anchor=CENTER, width=100)
-        #my_tree.column("Item Hired", anchor=W, width=120)
-        #my_tree.column("How many Items have you hired?", anchor=CENTER, width=80)
-        
-        #Headings
-        #my_tree.heading("Row #", text="Row", anchor=W)
-        #my_tree.heading("Your Name", text="Name", anchor=W)
-        #my_tree.heading("Reciept Number", text="Reciept no.", anchor=W)
-        #my_tree.heading("Item Hired", text="Item name", anchor=CENTER)
-        #my_tree.heading("How many Items have you hired?", text="Item No.", anchor=W)
-        #Data
-        
-        
-        #Labels and buttons for 2nd Window
-        label2=Label(root, text="Fill in the required fields", font=("Century Gothic",20), fg="red")
-        label2.place(relx=.0, rely=0.0, anchor='nw')
         
         #Destroy Program Function
         def close():
